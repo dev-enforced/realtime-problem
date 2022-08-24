1be95f5c7872a4888205f00f8af10f55729ef507
Roshan J <devenforced@gmail.com>
2022-08-25 04:14:47 +0530

    [refactor]: constants for filter based actions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,10 +1,1 @@
-const actionConstants = {
-  FILTERS: {},
-  SORTERS: {
-    SORT_BY_PRICE: {
-      HIGH_TO_LOW: "HIGH-T0-LOW",
-      LOW_TO_HIGH: "LOW-TO-HIGH",
-    },
-  },
-};
-export { actionConstants };
+export * from "./actionConstants";
